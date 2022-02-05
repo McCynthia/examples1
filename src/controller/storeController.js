3aabbecb826ede741090a0f1dc469cb5e3f4b5cf
Cynthia Meneses Cervon <CynthiaMC@MacBook-Pro.local>
2022-02-05 10:04:44 -0800

    store add-ons

--- src/controller/storeController.js
+++ src/controller/storeController.js
@@ -8,6 +8,51 @@ exports.store_get = function(req, res) {
 	res.status(200).json(store);
 }
 
+
+exports.store_list_type = function(req, res) {
+	const type = req.params.type;
+	consolo.log(type);
+	const found = store.find(element => element.type === type);
+	res.status(200).jason(found)
+}
+
+
+exports.store_post = function(req, res) {
+	const item = req.body;
+	console.log(item);
+	store.push(item);
+	console.log(store);
+	persist.save(store, storeFile);
+	res.status(200).json(store);
+}
+
+
+exports.store_post_type = function(req, res) {
+	const type = req.params.type;
+	const articulo = req.body;
+	const found = store.find(articulo => articulo.type === type);
+	if (typeof found != 'undefined') {
+		found.articulo.push(articulo);
+		rest.status(200).json(found);
+		persist.save(store, storeFile);
+	} else {
+		res.status(404).json('article not found');
+	}
+}
+
+
+exports.store_put_type = function(req, res) {
+	const item = req.body;
+	console.log(item);
+	const found = store.find(element => element.type === item.type);
+	if (typeof found != 'undefined') {
+		found.quantity = item.quantity;
+		res.status(200).json(store);
+		persis.save(store, storeFile);
+	} else {
+		res.status(404).json('item not found');
+	}
+}
 //
 // app.get('/food/dogs', function(req, res) {
 // 	// buscar los typos = dogs y regresarlos
--- src/routes.js
+++ src/routes.js
@@ -17,5 +17,9 @@ appRouter.post('/pets/:type', petsController.pet_put_type);
 appRouter.put('/pets', petsController.pet_put_type);
 
 appRouter.get('/store', storeController.store_get);
+appRouter.get('/store/:type', storeController.store_list_type);
+appRouter.post('/store', storeController.store_post);
+appRouter.post('/store/:type', storeController.store_put_type);
+appRouter.put('/store', storeController.store_put_type);
 
 module.exports = appRouter;
