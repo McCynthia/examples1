05e73b9ece78ffd2561b3ab23ff49e4e33847044
Claudia Cervon <ccervon@ccervonMacBookPro.local>
2022-02-05 10:49:49 -0800

    Adding member updates

--- src/controller/petsController.js
+++ src/controller/petsController.js
@@ -27,6 +27,7 @@ exports.pets_post_type = function(req, res) {
     const miembro = req.body;
     // Traverse array: pets.forEach(mascota => console.log(mascota));
     const found = pets.find(mascota => mascota.type === type);
+    console.log('Found %j', found);
     if (typeof found != 'undefined') {
         found.members.push(miembro);
         res.status(200).json(found);
@@ -37,9 +38,10 @@ exports.pets_post_type = function(req, res) {
 }
 
 exports.pet_put_type = function(req, res) {
+    const type = req.params.type;
     const pet = req.body;
     console.log(pet);
-    const found = pets.find(element => element.type === pet.type);
+    const found = pets.find(element => element.type === type);
     if (typeof found !== 'undefined') {
         found.quantity = pet.quantity;
         res.status(200).json(pets);
@@ -47,4 +49,25 @@ exports.pet_put_type = function(req, res) {
     } else {
         res.status(404).json('pet not found');
     }
+}
+
+exports.pet_put_type_name = function(req, res) {
+    const type = req.params.type;
+    const name = req.params.name;
+    console.log('Looking for %s in %s', name, type);
+    const pet = req.body;
+    console.log(pet);
+    const indexPetTypeFound = pets.findIndex(element => element.type === type);
+    if (indexPetTypeFound >= 0) {
+        console.log('Found %j', pets[indexPetTypeFound]);
+         const indexPetFound = pets[indexPetTypeFound].members.findIndex(element => element.name === name);
+        if (indexPetFound >= 0) {
+            console.log('Found %j', pets[indexPetTypeFound].members[indexPetFound]);
+            pets[indexPetTypeFound].members[indexPetFound] = {...pets[indexPetTypeFound].members[indexPetFound], ...pet };
+            res.status(200).json(pets);
+            persist.save(pets, petsFile);
+            return;
+        }
+    }
+    res.status(404).json('pet not found');
 }
--- src/routes.js
+++ src/routes.js
@@ -13,8 +13,9 @@ appRouter.get('/', function(req, res) {
 appRouter.get('/pets', petsController.pets_get);
 appRouter.get('/pets/:type', petsController.pets_list_type);
 appRouter.post('/pets', petsController.pets_post);
-appRouter.post('/pets/:type', petsController.pet_put_type);
-appRouter.put('/pets', petsController.pet_put_type);
+appRouter.post('/pets/:type', petsController.pets_post_type);
+appRouter.put('/pets/:type', petsController.pet_put_type);
+appRouter.put('/pets/:type/:name', petsController.pet_put_type_name);
 
 appRouter.get('/store', storeController.store_get);
 
